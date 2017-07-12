101ed4e6999feac7f7c2989669d1d4f153d40a58
nonocast <nonocast@gmail.com>
2017-07-12 22:47:30 +0800

    v0.1.0

--- api/src/main/java/cn/nonocast/api/ApiApplication.java
+++ api/src/main/java/cn/nonocast/api/ApiApplication.java
@@ -2,9 +2,17 @@ package cn.nonocast.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ApiApplication {
+
+	@RequestMapping("/")
+	public String home() {
+		return "api";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
---
+++ core/src/main/java/cn/nonocast/model/Meeting.java
@@ -0,0 +1,13 @@
+package cn.nonocast.model;
+
+public class Meeting {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
