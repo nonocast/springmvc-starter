6768253d7f8e44ff787d10fe1236e7ff51a062ea
nonocast <nonocast@gmail.com>
2017-07-12 22:05:23 +0800

    v0.1.0 multi-projects using gradle

---
+++ admin/src/main/java/cn/nonocast/admin/AdminApplication.java
@@ -0,0 +1,19 @@
+package cn.nonocast.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AdminApplication {
+	@RequestMapping("/")
+	public String home() {
+		return "admin";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminApplication.class, args);
+	}
+}
---
+++ core/src/main/java/cn/nonocast/model/User.java
@@ -0,0 +1,5 @@
+package cn.nonocast.model;
+
+public class User {
+
+}
