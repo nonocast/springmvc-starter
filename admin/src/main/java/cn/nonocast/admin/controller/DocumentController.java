dd7b9ff371d1d84d75aa3285d902a7ab57626f37
nonocast <nonocast@gmail.com>
2017-08-05 20:25:23 +0800

    add uplaod to gitignore

---
+++ admin/src/main/java/cn/nonocast/admin/controller/DocumentController.java
@@ -0,0 +1,7 @@
+package cn.nonocast.admin.controller;
+
+/**
+ * Created by nonocast on 8/5/17.
+ */
+public class DocumentController {
+}
---
+++ admin/src/main/java/cn/nonocast/admin/storage/StorageException.java
@@ -0,0 +1,12 @@
+package cn.nonocast.admin.storage;
+
+public class StorageException extends RuntimeException {
+
+    public StorageException(String message) {
+        super(message);
+    }
+
+    public StorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
